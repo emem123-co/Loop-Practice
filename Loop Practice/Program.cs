dfe640f7882a6362eb3db742b32a8166ca693f2d
Emily Morgan <emorgan921@gmail.com>
2024-06-12 16:11:55 -0400

    Add project files.

---
+++ Loop Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Loop_Practice;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+
+        for (var idx = 1; idx < 100; idx++)
+        {
+            sum = sum + idx;
+        }
+
+        Console.WriteLine($"The Sum of 1-100 is {sum}");
+    }
+}
+
